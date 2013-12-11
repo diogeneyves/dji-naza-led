99fef7a6d65f2722b261be8ea9abeb5506ced8dc
Leandre Gohy <leandre.gohy@hexeo.be>
2013-12-11 11:02:46 +0100

    Code rewriting and cleaning

--- src/helice_3.ino
+++ src/helice_3.ino
@@ -32,33 +32,27 @@ For more detail on the MIT license, see <http://opensource.org/licenses/MIT>
 
 #include <Adafruit_NeoPixel.h>
 #include <math.h>
-#define PIN 7
+
 #define ROTL(v, n) (v << n) | (v >> (sizeof(v) * 8 - n))
 #define ROTR(v, n) (v >> n) | (v << (sizeof(v) * 8 - n))
-#define RADIOIN1 13
-#define RADIOIN2 14
+
+#define PIN 7
+#define RADIOIN1 13 // 'gaz' command
+#define RADIOIN2 14 // animation selector
 #define FADING_STEP 5
-#define BRIGHT 30 //  brightness 0-255  
+#define BRIGHTNESS 30 // brightness 0-255  
+
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(64, PIN);
 word statesL = 0x303; // two arm define
 word statesR = 0x303; // two arm define
 //word statesL = 0xC63; // tree arm define
 //word statesR = 0xC63; // tree arm define
-long startTime;
-int sensorValue0;  // value coming from the Naza equal led green
-int sensorValue1;  // value coming from the Naza equal led red
-int duration; // read throtle
-int radiomin; // minimum throtle
-int turn = 100; // how fat turn led
-long color; // end color
-byte select; // select mode animation
-const int turns[] = {
-  0, 0, 0, 0, 0, 0, 320, 160, 80, 40, 20, 10}; // value by step 200 radio
+//int radiomin; // minimum throtle
 
 void setup() {
   Serial.begin(9600);
   pixels.begin();
-  pixels.setBrightness(30); //  brightness 0-255
+  pixels.setBrightness(BRIGHTNESS);
   pinMode(A4, INPUT);
   pinMode(A5, INPUT);
   pinMode(RADIOIN1, INPUT);
@@ -66,125 +60,100 @@ void setup() {
   //radiomin = pulseIn(RADIOIN, HIGH, 25000); // store min throtle
 }
 
-void loop() {
-  selectMode();
-  colors();
-  time();
-  radio();
+word getAnimationSpeed(void) {
+  static const int animationSpeeds[] = { 0, 0, 0, 0, 0, 0, 320, 160, 80, 40, 20, 10 }; // speeds of animation
+  // Read the radio 'gaz' command
+  unsigned long int duration = pulseIn(RADIOIN1, HIGH, 25000);
+  // Determine the animation speed based on the previous read and limite the index in range 0 - 11
+  byte animationSpeedIndex = (byte)(ceil((float)duration / 200.0f)) % 11;
+  // Select the animation speed
+  animationSpeed = animationSpeeds[animationSpeedIndex];
 }
 
-void radio() {
-  duration = pulseIn(RADIOIN1, HIGH, 25000);  // how fast led turn
-  int d = (int)ceil((float)duration / 200.0f);
-  turn = turns[((d <= 11) ? d : 11)];
-}
-
-void ring() {
-  int i;
-  int stateL;
-  int stateR;
-  if (turn == 0) {
-    neutre();
+unsigned long int getAnimationColor(word animationSpeed) {
+  static const unsigned long int animationColors[] = { 0x000000, 0x00FF00, 0xFF0000, 0xFF9900 }; // colors of animation
+  // read Naza red led
+  int sensorValue0 = analogRead(A4);
+  // read Naza green led
+  int sensorValue1 = analogRead(A5);
+  byte animationColorIndex = (sensorValue0 > 500) + ((sensorValue1 > 500) << 1);
+  if (animationColorIndex != 0 || animationSpeed != 0) {
+    animationColor = animationColors[animationColorIndex];
   }
-  else {
-    for (i = 0; i < 16; i++) {
-      stateL = (statesL >> i) & 0x0001;
-      stateR = (statesR >> i) & 0x0001;
-      pixels.setPixelColor(i, stateR * color);
-      pixels.setPixelColor(i+16, stateL * color);
-      pixels.setPixelColor(i+32, stateR * color);
-      pixels.setPixelColor(i+48, stateL * color);
+}
+
+void playAnimation(word animationSpeed, unsigned long int animationColor) {
+  static unsigned long int startTime = 0;
+  if (millis() - startTime >= animationSpeed) {
+    if (animationSpeed == 0) {
+      playNoAnimation(animationColor);
     }
-  }
-  statesL = ROTL(statesL, 1);
-  statesR = ROTR(statesR, 1);
-  //color = ROTL(color, 8); // change color
-  pixels.show();
-}
-
-void time() {
-  if (millis() - startTime >= turn) {
-    switch (select) {
-    case 1:
-      ring();
-      break;
-    case 2:
-      fading();
-      break;
+    else {
+      if (pulseIn(RADIOIN2, HIGH, 25000) < 1500) {
+        playAnimationRing(animationColor);
+      }
+      else {
+        playAnimationFading(animationColor);
+      }
     }
     startTime = millis();
   }
 }
 
-void colors() {
-  sensorValue0 = analogRead(A4); // read Naza red led
-  sensorValue1 = analogRead(A5); // read Naza green led
-  byte ledV = (sensorValue0 > 500);
-  byte ledR = (sensorValue1 > 500)<<1;
-  switch (ledR + ledV) {
-  case 0:
-    if (turn == 0) {
-      color = 0x000000; //none
-    }
-    break;
-  case 1:
-    color = 0x00FF00; //green
-    break;
-  case 2:
-    color = 0xFF0000; //red
-    break;
-  case 3:
-    color = 0xFF9900; //orange
-    break;
+void playNoAnimation(unsigned long int animationColor) {
+  for (byte i = 0; i < 16; i++) {
+    pixels.setPixelColor(i, animationColor);
+    pixels.setPixelColor(i+16, animationColor);
+    pixels.setPixelColor(i+32, animationColor);
+    pixels.setPixelColor(i+48, animationColor);
+    pixels.setBrightness(BRIGHTNESS);
   }
-}
-
-void fading() {
-  int i;
-  static int direction = 1; // start direction fading
-  static int intensity = 0;
-
-  if (turn == 0) { // throtle minimum
-    neutre();
-  }
-  else {
-    for (i = 0; i < 64; i++) 
-      pixels.setPixelColor(i, color);
-    if ( direction == 1) { 
-      intensity += FADING_STEP;               
-    }
-    if ( intensity > 250 ) { 
-      direction = -1;               
-    }
-    if ( direction == -1) { 
-      intensity -= FADING_STEP;               
-    }
-    if ( intensity < 5 ) { 
-      direction = 1;               
-    }
-    pixels.setBrightness(intensity); //  brightness
-  }
+  
   pixels.show();
 }
 
-void selectMode() {
-  select = pulseIn(RADIOIN2, HIGH, 25000);
-  if (select < 1500) {
-    select = 1;
+void playAnimationRing(unsigned long int animationColor) {
+  unsigned long int stateL = 0;
+  unsigned long int stateR = 0;
+  
+  for (byte i = 0; i < 16; i++) {
+    stateL = (statesL >> i) & 0x0001;
+    stateR = (statesR >> i) & 0x0001;
+    pixels.setPixelColor(i, stateR * animationColor);
+    pixels.setPixelColor(i+16, stateL * animationColor);
+    pixels.setPixelColor(i+32, stateR * animationColor);
+    pixels.setPixelColor(i+48, stateL * animationColor);
   }
-  else {
-    select = 2;
-  }
+  
+  statesL = ROTL(statesL, 1);
+  statesR = ROTR(statesR, 1);
+  
+  pixels.show();
 }
 
-void neutre() {
-  int i;
-  for (i = 0; i < 16; i++) {
-    pixels.setPixelColor(i, color);
-    pixels.setPixelColor(i+16, color);     
-    pixels.setPixelColor(i+32, color);     
-    pixels.setPixelColor(i+48, color); 
-    pixels.setBrightness(BRIGHT); //  brightness 0-255  
+void playAnimationFading(unsigned long int animationColor) {
+  static int direction = 1; // start direction fading
+  static int intensity = 5;
+
+  for (byte i = 0; i < 64; i++) {
+    pixels.setPixelColor(i, animationColor);
   }
+  
+  intensity += direction * FADING_STEP;
+  
+  if ( intensity > 250 ) {
+    direction = -1;
+  }
+  else if ( intensity < 5 ) {
+    direction = 1;
+  }
+  
+  pixels.setBrightness(intensity);
+  pixels.show();
 }
 
+void loop() {
+  word animationSpeed = getAnimationSpeed();
+  unsigned long int animationColor = getAnimationColor(animationSpeed);
+  playAnimation(animationSpeed, animationColor);
+}
